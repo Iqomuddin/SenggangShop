7162ce29e019c4582bcba6c38de28c186db796f2
Iqomuddin <134931625+Iqomuddin@users.noreply.github.com>
2025-08-06 19:27:51 +0700

    Add Vercel conf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: false,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/(.*)',
+        destination: '/',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
